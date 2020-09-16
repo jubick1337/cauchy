fed51d99265879e75f9d312841e4366d2cae3b24
matvey <mattyson.so@gmail.com>
2020-09-16 20:17:43 +0500

    google fucked up with multiply sign in russian so i made this:(

--- actions/simple_math.py
+++ actions/simple_math.py
@@ -7,6 +7,7 @@ class SimpleMathAction:
         self._regex = re.compile(r'[А-Яа-я ]*(\d+) ([+\-*/]) (\d+)')
 
     def get_result(self, query: str) -> Optional[int or float]:
+        query = query.replace('х', '*').replace('x', '*')
         try:
             groups = self._regex.match(query).groups()
             first_operand, operator, second_operand = groups
