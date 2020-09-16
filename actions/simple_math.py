87fdcfd30bc0c411487dd3d654ee4d1bf2ebb6cb
matvey <mattyson.so@gmail.com>
2020-09-16 19:28:56 +0500

    fixed bugs

--- actions/simple_math.py
+++ actions/simple_math.py
@@ -6,10 +6,10 @@ class SimpleMathAction:
     def __init__(self):
         self._regex = re.compile(r'[А-Яа-я ]*(\d+) ([+\-*/]) (\d+)')
 
-    def get_result(self, query: str) -> Optional[int, float]:
-        groups = self._regex.match(query).groups()
-        first_operand, operator, second_operand = groups
+    def get_result(self, query: str) -> Optional[int or float]:
         try:
+            groups = self._regex.match(query).groups()
+            first_operand, operator, second_operand = groups
             return eval(f'{first_operand}{operator}{second_operand}')
         except SyntaxError:
             return None
