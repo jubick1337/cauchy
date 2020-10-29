2e2cf86e3ebe431655d57d92dd48efd0e87cb4ce
matvey <mattyson.so@gmail.com>
2020-10-29 16:40:45 +0500

    added greeting intent

---
+++ actions/greeting.py
@@ -0,0 +1,17 @@
+import random
+import re
+from typing import Optional
+
+from actions.action import Action
+
+
+class Greeting(Action):
+    def __init__(self):
+        self._regex = re.compile(r'(Привет|Здравствуйте)')
+        self._answers = ['Привет', 'Хай', 'Рад тебя видеть']
+
+    def get_text(self, query: str) -> Optional[str]:
+        if self._regex.search(query):
+            return random.choice(self._answers)
+
+        return None
