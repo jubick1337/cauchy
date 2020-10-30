0a1c4fa990ce7c22d89118e92b5345e84f5197a9
matvey <mattyson.so@gmail.com>
2020-10-30 21:31:13 +0500

    upgraded google clud to v2

--- asr/google_asr_wrapper.py
+++ asr/google_asr_wrapper.py
@@ -1,30 +1,31 @@
 import io
 from pathlib import Path
 
-from google.cloud import speech_v1
-from google.cloud.speech_v1 import enums
+from google.cloud import speech
+# from google.cloud.speech_v1 import enums
 from google.oauth2 import service_account
 
 
 class GoogleASRWrapper:
     def __init__(self, credentials: Path):
         self._credentials = service_account.Credentials.from_service_account_file(credentials)
-        self._client = speech_v1.SpeechClient(credentials=self._credentials)
+        self._client = speech.SpeechClient(credentials=self._credentials)
         self._language_code = "ru-RU"
         self._sample_rate = 16000
-        self._encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+        self._encoding = speech.RecognitionConfig.AudioEncoding.LINEAR16
 
     def get_text(self, local_file_path):
-        config = {
-            "language_code": self._language_code,
-            "sample_rate_hertz": self._sample_rate,
-            "encoding": self._encoding,
-        }
         with io.open(local_file_path, "rb") as f:
             content = f.read()
-        audio = {"content": content}
+            audio = speech.RecognitionAudio(content=content)
 
-        response = self._client.recognize(config, audio)
+        config = speech.RecognitionConfig(
+            language_code=self._language_code,
+            sample_rate_hertz=self._sample_rate,
+            encoding=self._encoding,
+        )
+
+        response = self._client.recognize(config=config, audio=audio)
 
         for result in response.results:
             alternative = result.alternatives[0]
